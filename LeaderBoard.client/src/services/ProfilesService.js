3041fb2b10ca0492a9aa6ad3a22f94dca7aefa68
Casey Collins <collinscasey.homes@gmail.com>
2022-09-01 14:23:56 -0600

    commit

--- LeaderBoard.client/src/services/ProfilesService.js
+++ LeaderBoard.client/src/services/ProfilesService.js
@@ -1,9 +1,9 @@
 import { AppState } from "../AppState.js";
 import { api } from "./AxiosService.js";
 import { logger } from "../utils/Logger.js";
-import { Profile } from "models/Profile.js"
+// import { Profile } from "models/Profile.js"
 class ProfilesService {
-    async getProfileById(id) {
+    async getProfileBySearch(id) {
         const res = await api.get(`api/profiles/${id}`)
         logger.log('did this work?', res.data)
         AppState.activeProfile = new Profile(res.data)
