69de45905882279b1d184502d47eb49cebddb8e7
Casey Collins <collinscasey.homes@gmail.com>
2022-09-01 11:56:00 -0600

    commit

---
+++ LeaderBoard.client/src/pages/ProfilesPage.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div class="container-fluid bg-dark text-light">
+        <div class="row">
+            <div class="col-10">
+                <form @submit.prevent="handleSubmit">
+                    <label for="" class="form-label">Search Profiles</label>
+                    <input type="text" class="form-control" v-model="query">
+                    <button class="btn btn-info">Search</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+</template>
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+<style>
+</style>
