3c74aaabe02c7a1e5b1fba387d3a21e2beda341a
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-01 11:56:15 -0600

    home page in work

--- LeaderBoard.client/src/pages/HomePage.vue
+++ LeaderBoard.client/src/pages/HomePage.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+<body class="container-fluid">
+  <div class="row d-flex align-items-center justify-content-center">
+      <div class="col-6 card ">
+      <h1>Welcome to Trackr!</h1>
+      </div>
   </div>
+</body>
 </template>
 
 <script>
@@ -16,21 +15,16 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+body{
+    background-image: url(https://lh3.googleusercontent.com/EN9Fs_obpehxsm78L1JZVUsksxQ9nLd2d9BTiesICSVOhQ1exFbpuHs1Bzi9UdxExitnLSkJWUhR5_KYDe5iuR_5U3nT-AMtJKI6Aw=w600);
+    background-image: url(https://64.media.tumblr.com/f59867ea0781a951f5b51712e91ba01a/f145147b69889cf3-5e/s640x960/043bb2008715b303727281615d6ead2d12a8712a.gifv);
+    background-image: url(https://tuckerandmikesgnarlyextremeblog.files.wordpress.com/2017/03/9ca14e23ebbc1fc56d6ee16ef04c87f2_super-rare-8-bit-pepe-8-bit-space-background-gif_493-277.gif);
+    background-position: center;
+    background-size: cover;
+    height: 100vh
+    // height: 92.8vh;
+}
+.card{
+  background-color: rgb(195, 195, 195);
 }
 </style>
