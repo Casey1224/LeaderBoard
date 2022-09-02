fc02f6e0872c2219ae8b3a549ea6768193551f42
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-02 11:55:43 -0600

    ap[ush

--- LeaderBoard.client/src/pages/GameDetailsPage.vue
+++ LeaderBoard.client/src/pages/GameDetailsPage.vue
@@ -1,25 +1,24 @@
 <template>
     <div class="container-fluid">
         <div class="text-center">
-            <button class="btn btn-primary m-3" data-bs-toggle="modal" data-bs-target="#createMatchModal">Start Match</button>
+            <button class="btn btn-primary m-3" data-bs-toggle="modal" data-bs-target="#createMatchModal">Start
+                Match</button>
         </div>
-        <div class="row">
-            <div class="">
-                <div class="card rounded elevation-2" @click="goTo">
-                <div class="card-body">
-                <img class="img-fluid rounded" :src="game.img" alt="">
-            </div>
-        <div class="card-footer">
-            <h4>{{game.name}}</h4>
-            <p>{{game.minPlayer}} - {{game.maxPlayer}} Players</p>
-            <p>{{game.type}}</p>
-            <p>{{game.description}}</p>
-            <p>{{game.rules}}</p>
-        </div>
-        <div v-if="game.creatorId == account._id"> 
-                <i class="mdi mdi-delete-forever delete-button selectable" @click="deleteGame"></i>
-            </div>
-    </div>
+        <div class="row justify-content-center">
+            <div class="card rounded elevation-2 col-11" @click="goTo">
+                <div class="card-body justify-content-center d-flex">
+                    <img class="img-fluid rounded img" :src="game.img" alt="">
+                </div>
+                <div class="card-footer">
+                    <h1 class="text-center">{{ game.name }}</h1>
+                    <p>{{ game.minPlayer }} - {{ game.maxPlayer }} Players</p>
+                    <p>{{ game.type }}</p>
+                    <p>{{ game.description }}</p>
+                    <p>{{ game.rules }}</p>
+                </div>
+                <div v-if="game.creatorId == account._id">
+                    <i class="mdi mdi-delete-forever delete-button selectable" @click="deleteGame"></i>
+                </div>
             </div>
         </div>
     </div>
@@ -34,7 +33,7 @@
                 <div class="modal-body">
                     <form @submit.prevent="handleSubmit" class="row">
                         <div class="col-6">
-                            <label for="" class="form-label">Player 1 ({{account.name}})</label>
+                            <label for="" class="form-label">Player 1 ({{ account.name }})</label>
                             <input type="text" v-model="account.id" class="form-control" name="player1" id="player1"
                                 aria-describedby="nameHelp" required disabled>
                         </div>
@@ -45,7 +44,7 @@
                         </div>
                         <div class="col-12">
                             <label for="" class="form-label">Winner</label>
-                            <input type="text"  class="form-control" name="img" id="img" required>
+                            <input type="text" class="form-control" name="img" id="img" required>
                         </div>
                         <button type="submit" class="btn btn-primary m-2">Match Complete</button>
                     </form>
@@ -68,26 +67,26 @@ import { logger } from '../utils/Logger.js';
 import Pop from '../utils/Pop.js';
 
 export default {
-  setup() {
-    const query = ref('')
-    const router = useRouter()
-    const route = useRoute()
-    async function getById(){
-        try {
-            await gamesService.getById(route.params.gameId)
-        } catch (error) {
-            Pop.error('Getting Game By Id', error)
-            logger.error(error)
+    setup() {
+        const query = ref('')
+        const router = useRouter()
+        const route = useRoute()
+        async function getById() {
+            try {
+                await gamesService.getById(route.params.gameId)
+            } catch (error) {
+                Pop.error('Getting Game By Id', error)
+                logger.error(error)
+            }
         }
-    }
-    onMounted(() => {
-        getById()
-    })
-    return {
-        query,
-        game: computed(() => AppState.activeGame),
-        account: computed(() => AppState.account),
-        async deleteGame() {
+        onMounted(() => {
+            getById()
+        })
+        return {
+            query,
+            game: computed(() => AppState.activeGame),
+            account: computed(() => AppState.account),
+            async deleteGame() {
                 try {
                     const check = await Pop.confirm("Would you like to remove this game")
                     if (!check) {
@@ -100,11 +99,14 @@ export default {
                     Pop.error(error)
                 }
             }
+        }
     }
-  }
 }
 </script>
 
 
 <style lang="scss" scoped>
+.img {
+    max-height: 35vh;
+}
 </style>
