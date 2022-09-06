a03a826fb850a5893c1197399d8aa6a7a353d4c1
JackFox77 <jacobfox000@googlemail.com>
2022-09-06 14:45:13 -0600

    init

---
+++ LeaderBoard/server/controllers/FriendsController.js
@@ -0,0 +1,9 @@
+import BaseController from "../utils/BaseController"
+
+export class FriendsController extends BaseController {
+    constructor() {
+        super('api/friends')
+        this.router
+            .use
+    }
+}
