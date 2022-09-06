3742cebfa315795fa1c0e53336ba88d268829692
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-06 14:09:08 -0600

    retro

--- LeaderBoard.client/src/services/GamesService.js
+++ LeaderBoard.client/src/services/GamesService.js
@@ -4,45 +4,45 @@ import { api } from "./AxiosService.js"
 
 
 class GamesService {
-    async getAll(){
+    async getAll() {
         const res = await api.get('api/games')
         logger.log('Got Games', res.data)
         AppState.games = res.data
     }
-    async getById(gameId){
+    async getById(gameId) {
         const res = await api.get(`api/games/${gameId}`)
         logger.log('Got Game By Id', res.data)
         AppState.activeGame = res.data
     }
-    async createGame(newGame){
+    async createGame(newGame) {
         const res = await api.post('api/games', newGame)
         logger.log("create game", res.data)
         AppState.games.unshift(res.data)
     }
-    async deleteGame(gameId){
+    async deleteGame(gameId) {
         const res = await api.delete(`api/games/${gameId}`)
         logger.log('Canceling Game')
         AppState.games = AppState.games.filter(g => g.id != gameId)
     }
-    async editGame(gameData){
+    async editGame(gameData) {
         //TODO 
         const res = await api.put(`api/games/${gameData._id}`, gameData)
         logger.log('editing this game', res.data)
         let gameIndex = AppState.games.findIndex(g => g.id == gameData.id)
         AppState.games.splice(gameIndex, 1, res.data)
     }
-    async getGamesBySearch(searchTerm){
+    async getGamesBySearch(searchTerm) {
         logger.log("[Search Term]", searchTerm)
         const res = await api.get('api/games', {
             params: {
-                query: searchTerm
+                name: searchTerm
             }
         })
         logger.log('searching games', res.data)
         AppState.games = res.data
-        logger.log(AppState.games, 'found games') 
+        logger.log(AppState.games, 'found games')
     }
-    }
+}
 
 
 export const gamesService = new GamesService()
