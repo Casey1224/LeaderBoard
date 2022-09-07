417e9c980058436a99c1017babdb30883ae58819
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-07 16:04:22 -0600

    auth??

--- LeaderBoard.client/src/components/MatchProfileCard.vue
+++ LeaderBoard.client/src/components/MatchProfileCard.vue
@@ -3,18 +3,15 @@
 
     <div class="justify-content-evenly my-3">
         <img class="blah rounded-pill selectable  elevation-2 img-fluid" :src="profile.picture" alt=""
-            :title="profile.name" @click="selectPlayers">
-        <!-- <p>{{profile.name}}</p> -->
+            :title="profile.name" >
+        
     </div>
+   
 
 
 </template>
 <script>
-import { ref } from 'vue';
-import { matchesService } from '../services/MatchesService.js';
-import { profilesService } from '../services/ProfilesService.js';
-import { logger } from '../utils/Logger.js';
-import Pop from '../utils/Pop.js';
+
 
 
 
@@ -22,21 +19,10 @@ export default {
     props: {
         profile: { type: Object, required: true }
     },
-    setup(props) {
-        const newMatch = ref({ playerIds: [] }) //backend sending all the player ids to the server POST
-        const selectedPlayers = ref([]) //frontend sending the players to the "match page" 
+    setup() {
+
         return {
-            newMatch,
-            selectedPlayers,
-            async selectPlayers() {
-                let profileId = props.profile.id
-                newMatch.value.playerIds.push(profileId)
-                selectedPlayers.value.push(profileId)
-                logger.log("[NEW MATCH]", newMatch.value)
-                logger.log("[SELECTED PLAYERS]", selectedPlayers.value)
-                //TODO Pop confirm to add player
-                //TODO Second page of modal that as the selected player as well as account 
-            }
+            
         }
     }
 }
