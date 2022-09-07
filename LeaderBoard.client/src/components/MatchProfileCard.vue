012c27c170226283ccd377136cdc2de28012b445
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-07 10:56:41 -0600

    ama

---
+++ LeaderBoard.client/src/components/MatchProfileCard.vue
@@ -0,0 +1,44 @@
+<template>
+
+   
+        <div class="justify-content-evenly my-3">
+            <img class="blah rounded-pill selectable  elevation-2 img-fluid" :src="profile.picture" alt=""
+                :title="profile.name" @click="selectPlayers">
+                <!-- <p>{{profile.name}}</p> -->
+        </div>
+ 
+
+</template>
+<script>
+import { ref } from 'vue';
+import { matchesService } from '../services/MatchesService.js';
+import { logger } from '../utils/Logger.js';
+
+
+
+export default {
+    props: {
+        profile: { type: Object, required: true }
+    },
+    setup(props) {
+        const newMatch = ref({playerIds: []}) //backend sending all the player ids to the server POST
+        const selectedPlayers = ref([]) //frontend sending the players to the "match page" 
+        return {
+            newMatch,
+            async selectPlayers(){
+
+                logger.log('creating new match', newMatch.value)
+                // await matchesService.createMatch(newMatch.value)
+            }
+        }
+    }
+}
+</script>
+<style>
+.blah {
+    max-width: 95px;
+    max-height: 95px;
+    min-height: 95px;
+    min-width: 95px;
+}
+</style>
