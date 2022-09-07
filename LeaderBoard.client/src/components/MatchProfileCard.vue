8efa7927c079f8fb0f8c429b0610593313bf3616
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-07 11:53:09 -0600

    push

--- LeaderBoard.client/src/components/MatchProfileCard.vue
+++ LeaderBoard.client/src/components/MatchProfileCard.vue
@@ -1,18 +1,20 @@
 <template>
 
-   
-        <div class="justify-content-evenly my-3">
-            <img class="blah rounded-pill selectable  elevation-2 img-fluid" :src="profile.picture" alt=""
-                :title="profile.name" @click="selectPlayers">
-                <!-- <p>{{profile.name}}</p> -->
-        </div>
- 
+
+    <div class="justify-content-evenly my-3">
+        <img class="blah rounded-pill selectable  elevation-2 img-fluid" :src="profile.picture" alt=""
+            :title="profile.name" @click="selectPlayers">
+        <!-- <p>{{profile.name}}</p> -->
+    </div>
+
 
 </template>
 <script>
 import { ref } from 'vue';
 import { matchesService } from '../services/MatchesService.js';
+import { profilesService } from '../services/ProfilesService.js';
 import { logger } from '../utils/Logger.js';
+import Pop from '../utils/Pop.js';
 
 
 
@@ -21,14 +23,20 @@ export default {
         profile: { type: Object, required: true }
     },
     setup(props) {
-        const newMatch = ref({playerIds: []}) //backend sending all the player ids to the server POST
+        const newMatch = ref({ playerIds: [] }) //backend sending all the player ids to the server POST
         const selectedPlayers = ref([]) //frontend sending the players to the "match page" 
         return {
             newMatch,
-            async selectPlayers(){
+            selectedPlayers,
+            async selectPlayers() {
+                let profileId = props.profile.id
+                newMatch.value.playerIds.push(profileId)
+                selectedPlayers.value.push(profileId)
+                logger.log("[NEW MATCH]", newMatch.value)
+                logger.log("[SELECTED PLAYERS]", selectedPlayers.value)
 
-                logger.log('creating new match', newMatch.value)
-                // await matchesService.createMatch(newMatch.value)
+
+
             }
         }
     }
