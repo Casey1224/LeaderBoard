8e91b8e3de53d08f6bc4acf8f06011920494eaf3
JackFox77 <jacobfox000@googlemail.com>
2022-09-08 11:35:14 -0600

    init

--- LeaderBoard.client/src/components/MatchCard.vue
+++ LeaderBoard.client/src/components/MatchCard.vue
@@ -5,7 +5,7 @@
             <div class="col-8 ">
                 <div class="card  elevation-2 mx-2">
                     <h3>{{matches.game.name}}</h3>
-                    <img class="" :src="matches.game.coverImg" alt="">
+                    <img class="rounded" :src="matches.game.coverImg" alt="">
                     <div v-for="m in matches" :key="m.id">
                         {{matches.players.name}}
                     </div>
