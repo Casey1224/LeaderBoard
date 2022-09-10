cf55df3aa6f781509ad07bc8f77220929e3fc700
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-09 20:43:28 -0600

    leader

---
+++ LeaderBoard.client/src/components/LeaderBoardCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 leader"> </div>
+            
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { computed } from '@vue/reactivity';
+import { AppState } from '../AppState.js';
+
+export default {
+  setup() {
+    return {
+        profile: computed(() => AppState.profiles),
+        account: computed(() => AppState.account),
+        
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .leader{ 
+        background-color: #A8A8A8;
+    }
+</style> 
